9e97871bd9772d0e253c72e49a864192721206b3
Deqpwtf <prbaoleg@gmail.com>
2025-12-08 02:33:59 +0100

    initial config

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    semi: true,             // ставить точку с запятой
+    singleQuote: true,      // использовать одинарные кавычки
+    trailingComma: 'all',   // запятая в конце объектов и массивов
+    printWidth: 80,         // максимальная длина строки
+    tabWidth: 2,            // размер таба
+    endOfLine: "lf"
+};
